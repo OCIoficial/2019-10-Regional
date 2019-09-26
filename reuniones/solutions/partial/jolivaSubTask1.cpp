a6efd2929cdee408fbe2f916e5ee7af811d429cb
Javier Oliva <jav.oliva.silva@gmail.com>
2019-09-26 01:15:24 -0300

    Adding partial solutions

---
+++ reuniones/solutions/partial/jolivaSubTask1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    if (n==1){
+        cout << "1\n";
+        return 0;
+    }
+    pair<int, int>T1, T2;
+    cin >> T1.first >> T1.second >> T2.first >> T2.second;
+    if (T1>T2)
+        swap(T1, T2);
+    if (T2.first>T1.second)
+        cout << "1\n";
+    else
+        cout << "2\n";
+    return 0;
+}
