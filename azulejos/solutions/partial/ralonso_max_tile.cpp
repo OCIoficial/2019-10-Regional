f7f997f9b3ea8242e2d52b827df0254991668f8b
Rodrigo Alonso <rodrigo.alonso.o@gmail.com>
2019-09-25 22:12:48 -0700

    Updated testplan, added solutions (partial and correct)

---
+++ azulejos/solutions/partial/ralonso_max_tile.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  int m, n, p, q;
+  
+  int tile;
+  
+  int even = 0, odd = 0;
+
+  cin >> m >> n >> p >> q;
+
+  int cost = max(p, q);
+
+  for (int r = 0; r < m; r++) {
+    for (int c = 0; c < n; c++) {
+      cin >> tile;
+      if (tile ^ (r&1) ^ (c&1)) {
+        even += cost;
+      } else {
+        odd += cost;
+      }
+    }
+  }
+
+  cout << min(even, odd) << endl;
+
+  return 0;
+}
