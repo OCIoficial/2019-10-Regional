b41533b08ba32dc2412fa162c9005e05e574128f
Nico Lehmann <nico.lehmannm@gmail.com>
2019-09-28 17:47:42 -0500

    fix java solution

--- promedio/solutions/correct/Robin40.java
+++ promedio/solutions/correct/Robin40.java
@@ -1,12 +1,10 @@
-package promedio.solutions.correct;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Robin40 {
+class promedio {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String[] line;
